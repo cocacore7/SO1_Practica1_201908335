9f12056a9a24a4725b7a4ea17b2b255bb7f8acc0
Oscar Rodriguez <cocacore7@gmail.com>
2022-02-14 00:54:11 -0600

    Front Inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default App;
